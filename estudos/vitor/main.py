24c1123abf59ba88ea68dd8462978898738c3b47
AzaradoChan <chan.azarado@gmail.com>
2026-03-04 20:21:45 -0300

    feat: movimentação de imagem ao pressionar teclas

--- estudos/vitor/main.py
+++ estudos/vitor/main.py
@@ -10,32 +10,80 @@ DEGREES_DIR = {
 
 pygame.init()
 
+pygame.key.set_repeat(100, 10)
+
 screen = pygame.display.set_mode(SCREEN_SIZE)
-image = pygame.image.load("a.png").convert()
+image_main = pygame.image.load("a.png").convert()
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 
-image = pygame.transform.scale(image, (50, 50))
-image = pygame.transform.rotate(image, DEGREES_DIR["right"])
-image.set_colorkey((255, 255, 0))
+image_main = pygame.transform.scale(image_main, (50, 50))
+image_main.set_colorkey((255, 255, 0))
+image = pygame.transform.rotate(image_main, DEGREES_DIR["right"])
 
 delta_time = 0.1
-x= 0.1
+pos = [0.1, SCREEN_SIZE[1] // 2]
 velocity = 80
 running = True
 while running:
     screen.fill((0, 0, 0))
     
-    screen.blit(image, (x, SCREEN_SIZE[1] // 2))
+    screen.blit(image, (pos[0], pos[1]))
 
-    x += velocity * delta_time
+    hitbox = pygame.Rect(pos[0], pos[1], image.get_width(), image.get_height())
 
-    text = font.render(f"X: {x:.2f}", True, (255, 255, 255))
+    target = pygame.Rect(600, 300, 50, 50)
+
+    target_hit = target.colliderect(hitbox)
+    color = (0, 255, 0) if target_hit else (255, 0, 0)
+    pygame.draw.rect(screen, color, target, 2)
+
+    text = font.render(f"X: {pos[0]:.2f}, Y: {pos[1]:.2f}", True, (255, 255, 255))
     screen.blit(text, (10, 10))
 
     for event in pygame.event.get():
+        all_keys = pygame.key.get_pressed()
+        keys_pressed = {
+            "left": all_keys[pygame.K_LEFT], 
+            "right": all_keys[pygame.K_RIGHT], 
+            "up": all_keys[pygame.K_UP], 
+            "down": all_keys[pygame.K_DOWN]
+        }
         if event.type == pygame.QUIT:
             running = False
+        if sum(keys_pressed.values()) > 0:
+            
+            if keys_pressed["up"] and keys_pressed["right"]:
+                pos[1] -= velocity / 2 * delta_time
+                pos[0] += velocity / 2 * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["right"] + 45)
+            elif keys_pressed["up"] and keys_pressed["left"]:
+                pos[1] -= velocity / 2 * delta_time
+                pos[0] -= velocity / 2 * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["left"] - 45)
+            elif keys_pressed["down"] and keys_pressed["right"]:
+                pos[1] += velocity / 2 * delta_time
+                pos[0] += velocity / 2 * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["right"] - 45)
+            elif keys_pressed["down"] and keys_pressed["left"]:
+                pos[1] += velocity / 2 * delta_time
+                pos[0] -= velocity / 2 * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["left"] + 45)
+            elif keys_pressed["down"]:
+                pos[1] += velocity * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["down"])
+            elif keys_pressed["up"]:
+                pos[1] -= velocity * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["up"])
+            elif keys_pressed["right"]:
+                pos[0] += velocity * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["right"])
+            elif keys_pressed["left"]:
+                pos[0] -= velocity * delta_time
+                image = pygame.transform.rotate(image_main, DEGREES_DIR["left"])
+                
+                
+
 
     pygame.display.flip()
 
