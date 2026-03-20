a232b26d15d75aae93aa84f98399128a1da1cf4d
Vitor Garcia <vitorpso270@gmail.com>
2026-03-20 13:18:45 -0300

    fix: Correções para compatibilidade ao importar arquivos de imagem

--- src/utils/image.py
+++ src/utils/image.py
@@ -1,14 +1,17 @@
-from os import path
+import os
 from pathlib import Path
 
 import pygame
 
-def load_image(image_path: Path, size_x: int | None = None, size_y: int | None = None, scale: float | None = None) -> pygame.Surface:
+def load_image(image_path: os.PathLike | str, size_x: int | None = None, size_y: int | None = None, scale: float | None = None) -> pygame.Surface:
 
-    if not image_path.is_file():
+    if type(image_path) is str:
+        image_path = Path(image_path)
+
+    if type(image_path) is Path and not image_path.is_file():
         raise FileNotFoundError(f"Image file '{image_path}' not found.")
     
-    image = pygame.image.load(image_path).convert_alpha()
+    image = pygame.image.load(os.fspath(image_path)).convert_alpha()
     if size_x is not None and size_y is not None:
         image = pygame.transform.scale(image, (size_x, size_y))
     if scale is not None:
