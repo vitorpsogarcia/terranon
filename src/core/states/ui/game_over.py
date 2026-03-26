b9f0cfde92ac16213d780bada00d02f5885559dd
AzaradoChan <chan.azarado@gmail.com>
2026-03-25 21:49:15 -0300

    fix: fontes definidas no init

--- src/core/states/ui/game_over.py
+++ src/core/states/ui/game_over.py
@@ -11,7 +11,7 @@ class GameOverState(BaseState):
     def __init__(self, state_manager: "StateManager", screen_size: tuple[int, int]):
         super().__init__(state_manager, screen_size)
         self.font = pygame.font.SysFont("Arial", 72)
-        self.font = pygame.font.SysFont("Arial", 72)
+        self.small_font = pygame.font.SysFont("Arial", 36)
     
     def enter(self):
         print("Game Over State")
@@ -31,6 +31,6 @@ class GameOverState(BaseState):
     def draw(self, surface):
         surface.fill((0, 0, 0))
         game_over_text = self.font.render("GAME OVER", True, (255, 0, 0))
-        enter_to_continue = self.font.render("Press ENTER to continue", True, (255, 255, 255))
+        enter_to_continue = self.small_font.render("Press ENTER to continue", True, (255, 255, 255))
         surface.blit(game_over_text, (self.screen_size[0] // 2 - game_over_text.get_width() // 2, self.screen_size[1] // 2 - enter_to_continue.get_height() // 2))
-        surface.blit(enter_to_continue, (self.screen_size[0] // 2 - enter_to_continue.get_width() // 2, self.screen_size[1] // 2 + enter_to_continue.get_height() // 2))
+        surface.blit(enter_to_continue, (self.screen_size[0] // 2 - enter_to_continue.get_width() // 2, self.screen_size[1] // 2 + enter_to_continue.get_height() + 30))
