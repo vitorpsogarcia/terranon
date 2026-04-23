34f7454849f1975f0accedc91a40d4a9eb41bd9b
AzaradoChan <chan.azarado@gmail.com>
2026-04-22 22:31:43 -0300

    feat: Assets de tiro e utils de imagem e rotação

--- src/utils/image.py
+++ src/utils/image.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pygame
 
-def load_image(image_path: os.PathLike | str, size_x: int | None = None, size_y: int | None = None, scale: float | None = None) -> pygame.Surface:
+def load_image(image_path: os.PathLike | str, size: tuple[int, int] | None = None, scale: float | None = None) -> pygame.Surface:
 
     if type(image_path) is str:
         image_path = Path(image_path)
@@ -12,8 +12,8 @@ def load_image(image_path: os.PathLike | str, size_x: int | None = None, size_y:
         raise FileNotFoundError(f"Image file '{image_path}' not found.")
     
     image = pygame.image.load(os.fspath(image_path)).convert_alpha()
-    if size_x is not None and size_y is not None:
-        image = pygame.transform.scale(image, (size_x, size_y))
+    if size is not None:
+        image = pygame.transform.scale(image, size)
     if scale is not None:
         image = pygame.transform.scale(image, (int(image.get_width() * scale), int(image.get_height() * scale)))
     return image
