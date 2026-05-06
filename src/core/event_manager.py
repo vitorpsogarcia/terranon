45f8462ca4b803ce147f5993f70ccb7c65ba6689
AzaradoChan <chan.azarado@gmail.com>
2026-05-06 19:37:42 -0300

    fix: Correção no singleton do event_manager

--- src/core/event_manager.py
+++ src/core/event_manager.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 from core.enums.game_event_enum import GameEventEnum
 from core.exceptions.event_not_valid_exception import EventNotValidException
@@ -8,39 +8,38 @@ class EventManager:
     _instance = None
     _events_listeners: Dict[GameEventEnum, list] = {}
 
-    
-    @classmethod
-    def get_instance(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance = cls()
+            cls._instance = super().__new__(cls)
         return cls._instance
 
-    
+    @classmethod
+    def get_instance(cls):
+        return cls()
+
     @classmethod
     def initialize(cls):
-        if cls._instance is not None:
+        if getattr(cls, "_initialized", False):
             raise ValueError(f"{cls.__name__} has already been initialized.")
-        cls._instance = cls()
-
+        
+        instance = cls()
         for event in GameEventEnum:
-            cls._events_listeners[event] = []
+            instance._events_listeners[event] = []
+            
+        cls._initialized = True
 
-    
     def _validate_event(self, event: GameEventEnum):
         if event not in self._events_listeners:
             raise EventNotValidException(event.value)
-    
 
     def subscribe(self, event: GameEventEnum, listener):
         self._validate_event(event)
         self._events_listeners[event].append(listener)
-    
 
     def unsubscribe(self, event: GameEventEnum, listener):
         self._validate_event(event)
         self._events_listeners[event].remove(listener)
 
-    
     def emit(self, event: GameEventEnum, *args, **kwargs):
         self._validate_event(event)
         for listener in self._events_listeners[event]:
