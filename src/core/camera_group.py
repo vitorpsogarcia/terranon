5b24050d69de19c851e18a7e19f9351fd734bc80
Eduardo-BGarcia <eduardobgarciax134abc39@gmail.com>
2026-08-26 20:43:49 -0300

    fix: refatorado camera group;

--- src/core/camera_group.py
+++ src/core/camera_group.py
@@ -1,7 +1,5 @@
 import logging
-
 import pygame
-
 from core.game_object import GameObject, StaticObject
 from core.manager.debug_manager import DebugManager
 
@@ -23,7 +21,6 @@ class CameraGroup(pygame.sprite.LayeredUpdates):
     def target(self):
         if self._target is None:
             raise AttributeError("Target is not set for CameraGroup.")
-
         return self._target
 
     @target.setter
@@ -31,28 +28,27 @@ class CameraGroup(pygame.sprite.LayeredUpdates):
         self._target = target
 
     def custom_draw(self, surface: pygame.Surface):
-        if self.target.rect is None:
+        if self.target is None or not hasattr(self.target, 'pos'):
             return
 
-        self.offset.x = self.target.rect.centerx - self.half_w
-        self.offset.y = self.target.rect.centery - self.half_h
+        self.offset.x = self.target.pos.x - self.half_w
+        self.offset.y = self.target.pos.y - self.half_h
 
         target_pos, target_layer = self.target.pos, self.target.render_layer
 
         for sprite in self.sprites():
             owner = getattr(sprite, "owner", None)
             if owner is not None:
-                offset_pos = sprite.rect.topleft - self.offset
-
+                
                 if isinstance(owner, StaticObject):
                     distance = target_pos.distance_to(owner.pos)
-                    if distance < 200 and (
-                        target_layer - 75 < owner.render_layer < target_layer + 75
-                    ):
+                    if distance < 200 and (target_layer - 75 < owner.render_layer < target_layer + 75):
                         owner._sprite.opacity = 128
                     else:
                         owner._sprite.opacity = 255
 
-                surface.blit(sprite.image, offset_pos)
+                if hasattr(owner, "render_component") and owner.render_component is not None:
+                    owner.render_component.draw(surface, self.offset)
+                # ------------------------------------------
 
         DebugManager.draw_world_debug(surface, self)
