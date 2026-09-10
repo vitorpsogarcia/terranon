d62ba3ac338f871603c8d85c27dd36e3ea8c1c0b
Eduardo-BGarcia <eduardobgaricax134abc39@gmail.com>
2026-09-09 21:27:47 -0300

    feat(ui): adiciona UIPanel e consolida componentes genericos de UI (HU 3)

--- src/core/ui/__init__.py
+++ src/core/ui/__init__.py
@@ -1,5 +1,7 @@
 from core.ui.ui_element import UIElement
 from core.ui.button import Button
 from core.ui.text_input import TextInput
+from core.ui.panel import UIPanel, Panel
 
-__all__ = ["UIElement", "Button", "TextInput"]
+__all__ = ["UIElement", "Button", "TextInput", "UIPanel", "Panel"]
+
